3ff38265ba483e3aaf2203aa1036a1001a74370d
Sangwoo9652 <80228042+Sangwoo9652@users.noreply.github.com>
2021-03-22 10:15:52 +0900

    Add files via upload

---
+++ 0322/03_String.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace A003_String
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = "h";
+            b += "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
+        }
+    }
+}
---
+++ 0322/22_CompoundAssignment.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace A022_CompoundAssignment
+
+
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+            Console.WriteLine(x += 2);
+            Console.WriteLine(x -= 8);
+            Console.WriteLine(x *= 3);
+            Console.WriteLine(x /= 2);
+            Console.WriteLine(x++);
+            Console.WriteLine(--x);
+        }
+    }
+}
